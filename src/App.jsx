06667e4fa7dce1a0dad4276f8335105ca514e668
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-15 21:05:26 -0300

    Criação das páginas de registro e login

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 //pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 //components
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -18,6 +20,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
           </Routes>
         </div>
         <Footer />
