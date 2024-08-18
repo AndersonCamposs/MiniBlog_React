442589eb1a1f4c4cb2a5c14ac38b82267400cd5e
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-18 14:26:14 -0300

    Estilização da página estática Sobre

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,29 @@
 //CSS
 import styles from './About.module.css';
+import { Link } from 'react-router-dom';
+
+import { useAuthValue } from '../../context/AuthContext';
 
 const About = () => {
-  return <h1>AboutPage</h1>;
+  const { user } = useAuthValue();
+  return (
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>Este projeto consiste em um blog feito com React no front-end e Firebase no back-end</p>
+      {user && (
+        <Link to="/posts/create" className="btn">
+          Criar Post
+        </Link>
+      )}
+      {!user && (
+        <Link to="/register" className="btn">
+          Registre-se
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default About;
