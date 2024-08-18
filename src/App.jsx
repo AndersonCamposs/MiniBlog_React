35653b81dedbf5bbef60dc4a8ffb8231e13f4d67
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-18 14:11:57 -0300

    Criada a página DashBoard e de criação de posts, e adicionada a funcionalidade que bloqueia o acesso a essas páginas a usuários não autenticados.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
+
 //hooks
 import { useState, useEffect } from 'react';
 //custom-hooks
@@ -12,6 +13,8 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Dashboard from './pages/Dashboard/Dashboard';
+import CreatePost from './pages/CreatePost/CreatePost';
 //components
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -21,7 +24,6 @@ import { AuthProvider } from './context/AuthContext';
 function App() {
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
-  console.log(typeof user);
   const loadingUser = user === undefined;
 
   useEffect(() => {
@@ -34,15 +36,17 @@ function App() {
 
   return (
     <div className="App">
-      <AuthProvider value={user}>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <NavBar />
           <div className="container">
             <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About />}></Route>
-              <Route path="/login" element={<Login />}></Route>
-              <Route path="/register" element={<Register />}></Route>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+              <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+              <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/" />} />
+              <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
             </Routes>
           </div>
           <Footer />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
 //CSS
 import styles from './NavBar.module.css';
 
+import { useAuthValue } from '../context/AuthContext';
+import { useAuthentication } from '../hooks/useAuthentication';
+
 import { NavLink } from 'react-router-dom';
 const NavBar = () => {
+  const { user } = useAuthValue();
   return (
     <nav className={styles.navbar}>
       <NavLink to="/" className={styles.brand}>
@@ -14,16 +18,34 @@ const NavBar = () => {
             Home
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : '')}>
-            Entrar
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : '')}>
-            Registrar
-          </NavLink>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : '')}>
+                Entrar
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : '')}>
+                Registrar
+              </NavLink>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              <NavLink to="/posts/create" className={({ isActive }) => (isActive ? styles.active : '')}>
+                Novo post
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard" className={({ isActive }) => (isActive ? styles.active : '')}>
+                Dashboard
+              </NavLink>
+            </li>
+          </>
+        )}
         <li>
           <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : '')}>
             Sobre
