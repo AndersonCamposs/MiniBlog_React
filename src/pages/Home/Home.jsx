d81fb67825fc5641fd898a1b71f8ee64d3b130cc
AndersonCamposs <anderson.camposs87@outlook.com>
2024-08-19 23:01:33 -0300

    Criada a funcionalidade de listar todos os posts
    Foi desenvolvida a funcionalidade de listar todas as publicações feitas pelos usuários, na página home, além da estilização do componente que irá exibir essas publicações

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,44 @@
-//CSS
+// CSS
 import styles from './Home.module.css';
+// hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+//pages
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+// components
+import PostDetail from '../../components/PostDetail';
 
 const Home = () => {
-  return <h1>HomePage</h1>;
+  const [query, setQuery] = useState();
+  const { documents: posts, loading, error } = useFetchDocuments('posts');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail post={post} key={post.id} />)}
+        {posts && posts.length === 0 && (
+          <div>
+            <div className={styles.no_posts}>
+              <p>Não foram encontrados posts</p>
+              <Link to="/posts/create" className="btn">
+                Criar novo post
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
